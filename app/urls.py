17cc724150b2194d752c073eecccd4b964c001f8
pravin <impravin00@gmail.com>
2022-03-31 18:54:05 +0530

    Initial commit

---
+++ app/urls.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+from django import views
+from django.contrib import admin
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('index.html', views.index, name='index')
+]
